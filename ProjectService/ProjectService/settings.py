376733fb351d0bca4fdf63c6754f87cf8348fd41
Maria Enith Peralta <mosqueraperalta12@gmail.com>
2024-07-30 16:41:38 -0500

    settings.py

--- ProjectService/ProjectService/settings.py
+++ ProjectService/ProjectService/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-    STATICFILE_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
